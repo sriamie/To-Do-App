f657006264186fcbf9f2c778ed49d37f7aee7348
amie <sriamie123@gmail.com>
2020-07-12 18:05:29 -0700

    first commit

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const ToDoItem=(props) =>{
+    return (
+        <div>
+            <li>{props.text}
+
+            </li>
+        </div>
+
+    )
+}
+export default ToDoItem;
